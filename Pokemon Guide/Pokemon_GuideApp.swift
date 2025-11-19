e196de44b43ded9bc004d44b1c5353f9c302561f
Shayaan Siddiqui <shayaan.siddiqui@gmail.com>
2025-11-19 11:13:20 -0500

    Initial Commit

---
+++ Pokemon Guide/Pokemon_GuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokemon_GuideApp.swift
+//  Pokemon Guide
+//
+//  Created by Shayaan Siddiqui on 11/19/25.
+//
+
+import SwiftUI
+
+@main
+struct Pokemon_GuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
